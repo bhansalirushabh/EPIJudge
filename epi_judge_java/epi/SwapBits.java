84634656c8f75bacaa9d10ce118f9f54c2f24883
rbhansali9 <rbhansali9@gatech.edu>
2019-05-31 15:42:06 -0400

    4.2-SWAP-BITS

--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -1,11 +1,31 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+/**problem 4.2*/
 public class SwapBits {
+  public static int getBitAti(long x, int i){
+    while(i>0){
+      x = x>>1;
+      i--;
+    }
+    return (int)(x&1);
+  }
+  public static long flipBitAti(long x, int i){
+    long MASK = 1L << i;
+    return x^MASK;
+  }
+  /**optimal solution*/
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    // TODO - you fill in here.
-    return 0;
+    // COMPLETE - you fill in here.
+    int iBit = getBitAti(x,i);
+    int jBit = getBitAti(x,j);
+    if(iBit != jBit){
+      x = flipBitAti(x,i);
+      x = flipBitAti(x,j);
+    }
+
+    return x;
   }
 
   public static void main(String[] args) {
