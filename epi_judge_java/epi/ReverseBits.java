35fb5ff8898e3e7c9916d8df56eda0066302febc
rbhansali9 <rbhansali9@gatech.edu>
2019-05-31 16:04:14 -0400

    4.3-REVERSE_BITS

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -5,7 +5,16 @@ public class ReverseBits {
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
     // TODO - you fill in here.
-    return 0;
+    long ret = 0L;
+    int n = Long.SIZE;
+    while(n>0){
+      int tempBit = (int) (x &(1L));
+      x = x>>1;
+      ret=ret<<1;
+      ret=ret|tempBit;
+      n -= 1;
+    }
+    return ret;
   }
 
   public static void main(String[] args) {
